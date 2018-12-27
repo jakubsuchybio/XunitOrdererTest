bd83242225bc7b2521f7b13cd5d58884845d03a3
Kuba <suchyj@btlnet.com>
2018-12-27 14:26:31 +0100

    Add project files.

---
+++ PriorityBaseClass.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using XUnitPriorityOrderer;
+
+// set to be sequential execution
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+// addset the custom test's collection orderer
+[assembly: TestCollectionOrderer(CollectionPriorityOrderer.TypeName, CollectionPriorityOrderer.AssembyName)]
+
+namespace XunitOrdererTest
+{
+    // set the custom test's case orderer
+    [TestCaseOrderer(CasePriorityOrderer.TypeName, CasePriorityOrderer.AssembyName)]
+    public class PriorityBaseClass { }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Xunit;
+using XUnitPriorityOrderer;
+
+namespace XunitOrdererTest
+{
+    [Order(1)]
+    public class TestClass1 : PriorityBaseClass
+    {
+        [Fact]
+        [Order(1)]
+        public void TestMethod1()
+        {
+            Thread.Sleep(1000);
+            Debug.WriteLine(1);
+        }
+
+        [Fact]
+        [Order(2)]
+        public void TestMethod2()
+        {
+            Thread.Sleep(1000);
+            Debug.WriteLine(2);
+        }
+    }
+
+    [Order(2)]
+    public class TestClass2 : PriorityBaseClass
+    {
+        [Fact]
+        [Order(3)]
+        public void TestMethod1()
+        {
+            Thread.Sleep(1000);
+            Debug.WriteLine(3);
+        }
+
+        [Fact]
+        [Order(4)]
+        public void TestMethod2()
+        {
+            Thread.Sleep(1000);
+            Debug.WriteLine(4);
+        }
+    }
+
+    [Order(3)]
+    public class TestClass3 : PriorityBaseClass
+    {
+        [Fact]
+        [Order(5)]
+        public void TestMethod1()
+        {
+            Thread.Sleep(1000);
+            Debug.WriteLine(3);
+        }
+
+        [Fact]
+        [Order(6)]
+        public void TestMethod2()
+        {
+            Thread.Sleep(1000);
+            Debug.WriteLine(4);
+        }
+    }
+
+    [Order(4)]
+    public class TestClass4 : PriorityBaseClass
+    {
+        [Fact]
+        [Order(7)]
+        public void TestMethod1()
+        {
+            Thread.Sleep(1000);
+            Debug.WriteLine(3);
+        }
+
+        [Fact]
+        [Order(8)]
+        public void TestMethod2()
+        {
+            Thread.Sleep(1000);
+            Debug.WriteLine(4);
+        }
+    }
+}
